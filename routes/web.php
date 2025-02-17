a690ab2e3dc56ffcaeada08e60e182a93e0eb6f6
ABDELKEBIR HADOUI <abdelkebir123123@gmail.com>
2025-02-17 17:02:49 +0100

    Rapport_02/17/2025

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::view('/about', 'about');
+
+Route::get('/contact/{name}', function (string $name) {
+    return view('contact', ['name' => $name]);
+});
